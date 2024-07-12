51c0806ccd6a416e4035f211f63b1b6c5c1936d4
snehajose31 <120885046+snehajose31@users.noreply.github.com>
2024-07-12 21:51:43 +0530

    final_code

--- code.py
+++ code.py
@@ -1,13 +1,20 @@
-for i in range(0,4):
-    for k in range(0,8):
+value = input("Enter Two Number Sperated by space")
+num = value.split()
+x = int(num[0])
+y = int(num[1])
+for i in range(0,x):
+    for k in range(0,y+1):
         if k % 2 != 0:
             print(" ___",end='      ')
     print()
-    for j in range(0,4):
+    for j in range(0,x):
         print("/ ",end='   ')
-        print("\ ", end='   ')
+        print("\ ", end= '   'if j== x-1  else '___')
     print()
-    for j in range(0, 4):
+    for j in range(0, x):
         print("\ ", end='   ')
-        print("/ ", end='   ')
+        print("/ ", end='   ')
     print()
+for k in range(0,y+1):
+    if k % 2 != 0:
+        print(" ___",end='      ') 
